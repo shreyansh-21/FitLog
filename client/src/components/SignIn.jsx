965a057cc3d016d60a8d4b9a3d734b8502d83639
Shreyansh <shreyansh2128@gmail.com>
2025-02-23 19:13:24 +0530

    final commit

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -61,7 +61,7 @@ const SignIn = () => {
   return (
     <Container>
       <div>
-        <Title>Welcome to Fittrack 👋</Title>
+        <Title>Welcome to Fit-Log 👋</Title>
         <Span>Please login with your details here</Span>
       </div>
       <div
