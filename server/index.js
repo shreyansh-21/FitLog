fcdf130352e62a741197d3b4ca5bca5b3202aa14
Shreyansh <shreyansh2128@gmail.com>
2025-02-27 10:39:38 +0530

    deploy commits

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
