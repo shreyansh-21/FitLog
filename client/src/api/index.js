d71d9990dd05cdae911833e0e3dfa3875fbcb849
Shreyansh <shreyansh2128@gmail.com>
2025-03-01 14:56:16 +0530

    deploy updates

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "fit-log-three.vercel.app/api",
+  baseURL: "https://fitlog-y59t.onrender.com/api",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
