25e74cd6511402bdba2aa6b014f1423ed1c260ac
Shreyansh <shreyansh2128@gmail.com>
2025-03-01 15:03:13 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -8,9 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
+  origin: "https://fitlog-omega.vercel.app"
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
